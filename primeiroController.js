ca2de30c4b0835e744375c2c9feb3bdbeb99cedc
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-08-21 09:23:49 -0300

    update

--- primeiroController.js
+++ primeiroController.js
@@ -7,13 +7,19 @@ app.controller('primeiroController', ['$scope', function($scope) {
     console.log($scope.bt_subtrai);
 
     $scope.user = [
-                    { meuNome : 'Marcelo Neri da Silva'},
-                    { meuNome : 'Heliane Medeiros da Silveira e Silva'}
+                    { id : 1, meuNome : 'Marcelo Neri da Silva'},
+                    { id : 2, meuNome : 'Heliane Medeiros da Silveira e Silva'}
                 ];
 
-    $scope.contador = 0;
+    $scope.obj = {};
+    $scope.contador = 2;
 
     $scope.soma = function(){
+        $scope.obj.id = $scope.contador;
+        $scope.obj.meuNome = "Helena Medeiros da Silveira";
+        $scope.user.push($scope.obj);
+        console.log('Objeto temporário: '+$scope.obj);
+        $scope.obj = {};
         if($scope.contador > -1){
             $scope.bt_subtrai.removeAttribute('disabled');
         }        
@@ -23,7 +29,7 @@ app.controller('primeiroController', ['$scope', function($scope) {
     
 
     $scope.subtrai = function(){
-        if($scope.contador < 2){
+        if($scope.contador == 0){
             $scope.bt_subtrai.setAttribute('disabled', true);
             return;
         } else {
