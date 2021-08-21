e4cf2699d4cece99f900929310f02e05bd754d5f
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-08-21 01:49:33 -0300

    update

--- primeiroController.js
+++ primeiroController.js
@@ -2,6 +2,10 @@ var app = angular.module('loja', []);
 
 
 app.controller('primeiroController', ['$scope', function($scope) {
+
+    $scope.bt_subtrai = document.querySelector('.bt-subtrai');
+    console.log($scope.bt_subtrai);
+
     $scope.user = [
                     { meuNome : 'Marcelo Neri da Silva'},
                     { meuNome : 'Heliane Medeiros da Silveira e Silva'}
@@ -10,13 +14,21 @@ app.controller('primeiroController', ['$scope', function($scope) {
     $scope.contador = 0;
 
     $scope.soma = function(){
+        if($scope.contador > -1){
+            $scope.bt_subtrai.removeAttribute('disabled');
+        }        
         $scope.contador++;
     }
 
     
 
     $scope.subtrai = function(){
-        $scope.contador--;
+        if($scope.contador < 2){
+            $scope.bt_subtrai.setAttribute('disabled', true);
+            return;
+        } else {
+            $scope.contador--;
+        }
     }
 
 }]);
